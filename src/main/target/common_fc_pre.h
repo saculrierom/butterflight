409e47916b56a08fc78b4a5f3ff57ac362a22e8c
Lucas Moreira <lucas@moreiradevelopment.io>
2018-02-23 08:20:44 -0500

    Restructured common_fc_pre to fit FKF on F3 targets

--- src/main/target/common_fc_pre.h
+++ src/main/target/common_fc_pre.h
@@ -130,7 +130,7 @@
 #define USE_SERIALRX_SBUS       // Frsky and Futaba receivers
 #define USE_SERIALRX_SPEKTRUM   // SRXL, DSM2 and DSMX protocol
 #define USE_SERIALRX_SUMD       // Graupner Hott protocol
-#define USE_SERIALRX_SUMH       // Graupner legacy protocol
+//#define USE_SERIALRX_SUMH       // Graupner legacy protocol
 #define USE_SERIALRX_XBUS       // JR
 
 
@@ -143,18 +143,22 @@
 
 #if (FLASH_SIZE > 64)
 #define USE_BLACKBOX
-#define USE_LED_STRIP
+//#define USE_LED_STRIP
 #define USE_RESOURCE_MGMT
 #define USE_RUNAWAY_TAKEOFF     // Runaway Takeoff Prevention (anti-taz)
-#define USE_SERVOS
+//#define USE_SERVOS
 #define USE_TELEMETRY
 #define USE_TELEMETRY_FRSKY_HUB
 #define USE_TELEMETRY_HOTT
 #define USE_TELEMETRY_LTM
+#define USE_GYRO_FAST_KALMAN
 #define USE_TELEMETRY_SMARTPORT
 #endif
 
 #if (FLASH_SIZE > 128)
+#define USE_SERIALRX_SUMH       // Graupner legacy protocol
+#define USE_SERVOS
+#define USE_LED_STRIP
 #define USE_CAMERA_CONTROL
 #define USE_CMS
 #define USE_COPY_PROFILE_CMS_MENU
@@ -211,5 +215,5 @@
 #define USE_TELEMETRY_JETIEXBUS
 #define USE_TELEMETRY_MAVLINK
 #define USE_UNCOMMON_MIXERS
-#define USE_GYRO_FAST_KALMAN
+//#define USE_GYRO_FAST_KALMAN
 #endif
