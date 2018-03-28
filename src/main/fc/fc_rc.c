5c82c6a9094ce354fddec7b44664bc3dcd1c8db6
Tim Sweet <timothyswt@gmail.com>
2018-03-27 21:21:13 -0700

    fixing step counting outside of loop. saving cycles

--- src/main/fc/fc_rc.c
+++ src/main/fc/fc_rc.c
@@ -58,6 +58,7 @@ static float rcCommandInterp[4] = { 0, 0, 0, 0 };
 static float rcStepSize[4] = { 0, 0, 0, 0 };
 static float inverseRcInt;
 static int16_t rcInterpolationStepCount;
+static int16_t rcInterpolationStepCountCurrent;
 static uint16_t rxRefreshRate;
 volatile uint16_t currentRxRefreshRate;
 
@@ -210,19 +211,20 @@ FAST_CODE NOINLINE void processRcCommand(void)
                 //debug[1] = lrintf(rcCommandInterp[0]);
                 //debug[1] = lrintf(rcStepSize[0]*100);
             }
+            rcInterpolationStepCountCurrent = rcInterpolationStepCount;
         } else {
-            rcInterpolationStepCount--;
+            rcInterpolationStepCountCurrent--;
         }
 
         // Interpolate steps of rcCommand
-        if (rcInterpolationStepCount > 0) {
+        if (rcInterpolationStepCountCurrent > 0) {
             for (updatedChannel = ROLL; updatedChannel < interpolationChannels; updatedChannel++) {
                 rcCommandInterp[updatedChannel] += rcStepSize[updatedChannel];
                 rcCommand[updatedChannel] = rcCommandInterp[updatedChannel];
             }
         }
     } else {
-        rcInterpolationStepCount = 0; // reset factor in case of level modes flip flopping
+        rcInterpolationStepCountCurrent = 0; // reset factor in case of level modes flip flopping
     }
 
     if (isRXDataNew || updatedChannel) {
@@ -239,7 +241,7 @@ FAST_CODE NOINLINE void processRcCommand(void)
         }
 
         if (debugMode == DEBUG_RC_INTERPOLATION) {
-            debug[2] = rcInterpolationStepCount;
+            debug[2] = rcInterpolationStepCountCurrent;
             debug[3] = setpointRate[0];
         }
 
@@ -401,6 +403,6 @@ void initRcProcessing(void)
             rxRefreshRate = rxGetRefreshRate();
     }
 
-    rcInterpolationStepCount = rxRefreshRate / constrain(targetPidLooptime, 62.5f, 5000);
+    rcInterpolationStepCount = rxRefreshRate / constrainf(targetPidLooptime, 125.0f, 5000.0f);
     inverseRcInt = 1.0f / (float)rcInterpolationStepCount;
 }
