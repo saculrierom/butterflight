ButterFlight/betaflight

Setting FKF As default with better values & removing TAZ

src/main/sensors/gyro.c
@@ -170,16 +170,16 @@ PG_RESET_TEMPLATE(gyroConfig_t, gyroConfig,
     .gyro_high_fsr = false,
     .gyro_use_32khz = false,
     .gyro_to_use = 0,
-    .gyro_soft_notch_hz_1 = 400,
-    .gyro_soft_notch_cutoff_1 = 300,
-    .gyro_soft_notch_hz_2 = 200,
-    .gyro_soft_notch_cutoff_2 = 100,
-    .checkOverflow = GYRO_OVERFLOW_CHECK_ALL_AXES,
+    .gyro_soft_notch_hz_1 = 0,
+    .gyro_soft_notch_cutoff_1 = 0,
+    .gyro_soft_notch_hz_2 = 0,
+    .gyro_soft_notch_cutoff_2 = 0,
+    .checkOverflow = OFF,
     .gyro_soft_lpf_hz_2 = 0,
-    .gyro_filter_q = 0,
-    .gyro_filter_r = 0,
+    .gyro_filter_q = 400,
+    .gyro_filter_r = 88,
     .gyro_filter_p = 0,
-    .gyro_stage2_filter_type = STAGE2_FILTER_NONE,
+    .gyro_stage2_filter_type = FAST_KALMAN,
     .gyro_offset_yaw = 0,
 );
 

saculrierom/butterflight

src/main/sensors/gyro.c
@@ -216,16 +216,16 @@ PG_RESET_TEMPLATE(gyroConfig_t, gyroConfig,
     .gyro_high_fsr = false,
     .gyro_use_32khz = false,
     .gyro_to_use = GYRO_CONFIG_USE_GYRO_DEFAULT,
-    .gyro_soft_notch_hz_1 = 400,
-    .gyro_soft_notch_cutoff_1 = 300,
-    .gyro_soft_notch_hz_2 = 200,
-    .gyro_soft_notch_cutoff_2 = 100,
+    .gyro_soft_notch_hz_1 = 0,
+    .gyro_soft_notch_cutoff_1 = 0,
+    .gyro_soft_notch_hz_2 = 0,
+    .gyro_soft_notch_cutoff_2 = 0,
     .checkOverflow = GYRO_OVERFLOW_CHECK_ALL_AXES,
     .gyro_soft_lpf_hz_2 = 0,
-    .gyro_filter_q = 0,
-    .gyro_filter_r = 0,
+    .gyro_filter_q = 400,
+    .gyro_filter_r = 88,
     .gyro_filter_p = 0,
-    .gyro_stage2_filter_type = STAGE2_FILTER_NONE,
+    .gyro_stage2_filter_type = STAGE2_FILTER_FAST_KALMAN,
     .gyro_offset_yaw = 0,
     .gyro_lma_depth = 0,
     .gyro_lma_weight = 100,